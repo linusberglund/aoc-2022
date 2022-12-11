fa04eb84869f825248070047f149b28fcf20d5c5
Linus Berglund <linus@berglund.me>
2022-12-11 21:13:55 +0100

    solve day09 WIP 2

--- src/day09/solution.py
+++ src/day09/solution.py
@@ -1,7 +1,3 @@
-# isort --profile black . && black .
-# python .\src\day09\solution.py
-# watchexec -- "cls && python .\src\day09\solution.py"
-
 from pathlib import Path
 from pprint import pp
 
@@ -9,172 +5,151 @@ from aocd import submit
 
 ################################################################################
 
-# def debug(data):
-#     pass
 
-# def get_dimensions(pos_list):
-#     min_x = 0
-#     max_x = 0
-#     min_y = 0
-#     max_y = 0
+def debug(history):
+    min_x = 0
+    max_x = 0
 
-#     for pos in pos_list:
-#         print(pos)
-#         if pos[0] >= max_x:
-#             max_x = pos[0]
-#         if pos[0] <= min_x:
-#             min_x = pos[0]
-#         if pos[1] >= max_y:
-#             max_y = pos[1]
-#         if pos[1] <= min_y:
-#             min_y = pos[1]
+    min_y = 0
+    max_y = 0
 
-#     print(min_x, max_x, min_y, max_y)
+    for x, y in history:
+        if min_x > x:
+            min_x = x
+        if x > max_x:
+            max_x = x
 
-#     return (min_x, max_x, min_y, max_y)
+        if min_y > y:
+            min_y = y
+        if y > max_y:
+            max_y = y
 
-# def get_position_history(data):
-#     pos_list = []
-#     pos_list.append([0,0])
+    x_offset = 0 - min_x
+    y_offset = 0 - min_y
 
-#     for x, y in data:
-#         pos = [pos_list[-1][0], pos_list[-1][1]]
-#         match x, y:
-#             case "R", y:
-#                 pos[0] += y
-#             case "L", y:
-#                 pos[0] -= y
-#             case "U", y:
-#                 pos[1] += y
-#             case "D", y:
-#                 pos[1] -= y
-#         pos_list.append(pos)
+    x_range = range(0, x_offset + max_x + 2)
+    y_range = range(0, y_offset + max_y + 1)
 
-#     return pos_list
+    test = []
 
-# def create_board(dimensions):
-#     min_x, max_x, min_y, max_y = dimensions
+    for y in y_range:
+        line = ""
+        for x in x_range:
+            if (x - x_offset, y - y_offset) in history:
+                line += "#"
+            else:
+                line += "."
 
-#     # []
-#     # data = [[int(n) for n in line] for line in data]
-#     test = [["." for x in range(min_x, max_x+1)] for y in range(min_y, max_y+1)]
-#     pp(test)
+        test.append(line)
 
-#     # for x in range(min_x, max_x+1):
-#     #     for y in range(min_y, max_y+1):
+    test = test[::-1]
+    for line in test:
+        print("".join(line))
 
 
-#     pass
+def step(next_dir, head_x, head_y, tail_x, tail_y):
+    x = head_x
+    y = head_y
 
-# def part1(data):
-#     pp(data)
-#     l = get_position_history(data)
-#     dim = get_dimensions(l)
-#     create_board(dim)
+    match next_dir:
+        case "R":
+            head_x += 1
+        case "L":
+            head_x -= 1
+        case "U":
+            head_y += 1
+        case "D":
+            head_y -= 1
 
-#     return None
+    if abs(head_x - tail_x) > 1 or abs(head_y - tail_y) > 1:
+        tail_x = x
+        tail_y = y
 
-def step(prev_dir, next_dir, tail_x, tail_y):
-    match prev_dir, next_dir:
-        case "", "R":
-            tail_x += 1
-        case "", "L":
-            tail_x -= 1
-        case "", "U":
-            tail_y += 1
-        case "", "D":
-            tail_y -= 1
+    return head_x, head_y, tail_x, tail_y
 
-        case "R", "R":
-            tail_x += 1
-        case "R", "L":
-            pass
-        case "R", "U":
-            tail_x += 1
-            tail_y += 1
-        case "R", "D":
-            tail_x += 1
-            tail_y -= 1
-
-        case "L", "R":
-            pass
-        case "L", "L":
-            tail_x -= 1
-        case "L", "U":
-            tail_x -= 1
-            tail_y += 1
-        case "L", "D":
-            tail_x -= 1
-            tail_y -= 1
-
-        case "U", "R":
-            tail_x += 1
-            tail_y += 1
-        case "U", "L":
-            tail_x -= 1
-            tail_y += 1
-        case "U", "U":
-            tail_y += 1
-        case "U", "D":
-            pass
-
-        case "D", "R":
-            tail_x += 1
-            tail_y -= 1
-        case "D", "L":
-            tail_x -= 1
-            tail_y -= 1
-        case "D", "U":
-            pass
-        case "D", "D":
-            tail_y -= 1
-
-    return tail_x, tail_y
 
 def part1(data):
-    data = data[:5]
-    # pp(data)
+    head_x = 0
+    head_y = 0
 
     tail_x = 0
     tail_y = 0
-    prev_dir = ""
 
     tail_history = []
 
     for next_dir, y in data:
-        print(prev_dir, next_dir, y)
-        for _ in range(y-1):
-            tail_x, tail_y = step(prev_dir, next_dir, tail_x, tail_y)
+        for _ in range(y):
+            head_x, head_y, tail_x, tail_y = step(
+                next_dir,
+                head_x,
+                head_y,
+                tail_x,
+                tail_y,
+            )
             tail_history.append((tail_x, tail_y))
-            prev_dir = next_dir
-        print(tail_history)
 
-    # print(tail_history)
-
-
-    test = [["." for x in range(6)] for y in range(5)]
-    count = 0
-    for x,y in tail_history:
-        # test[x][y] = "#"
-        test[y][x] = "#"
-        count += 1
-    # for x in len(test):
-    #     for y in len(test[0]):
-
-    pp(test[::-1])
-
-    result = set(tail_history)
-    print(count, len(tail_history))
-    print(count, len(result))
-
-    return len(result)
+    tail_history = set(tail_history)
+    return len(tail_history)
 
 
 ################################################################################
 
 
+def step_dynamic_size(next_dir, head_x, head_y, tail_x, tail_y, size):
+    x = head_x
+    y = head_y
+
+    match next_dir:
+        case "R":
+            head_x += 1
+        case "L":
+            head_x -= 1
+        case "U":
+            head_y += 1
+        case "D":
+            head_y -= 1
+
+    if abs(head_x - tail_x) > size or abs(head_y - tail_y) > size:
+        tail_x = x
+        tail_y = y
+
+    return head_x, head_y, tail_x, tail_y
+
+
 def part2(data):
-    return None
+    head_x = 0
+    head_y = 0
+
+    tail_x = 0
+    tail_y = 0
+
+    rope_length = 10
+
+    head_history = []
+    tail_history = []
+
+    for next_dir, y in data:
+        for _ in range(y):
+            head_x, head_y, tail_x, tail_y = step_dynamic_size(
+                next_dir,
+                head_x,
+                head_y,
+                tail_x,
+                tail_y,
+                rope_length,
+            )
+            head_history.append((head_x, head_y))
+            tail_history.append((tail_x, tail_y))
+
+    head_history = set(head_history)
+    tail_history = set(tail_history)
+
+    debug(head_history)
+    # debug(tail_history)
+
+    print(f"Total: {len(tail_history)}, Expected: {36}")
+
+    return len(tail_history)
 
 
 ################################################################################
@@ -189,7 +164,7 @@ def parse(path):
 
 def auto_submitter():
     example_part1 = 13
-    example_part2 = False
+    example_part2 = 36
 
     if example_part1 == part1(parse("src/day09/example.txt")):
         print(f"PART 1 EXAMPLE PASS ({example_part1}), SUBMITTING")
@@ -202,6 +177,7 @@ def auto_submitter():
 
 data = "src/day09/input.txt"
 data = "src/day09/example.txt"
+data = "src/day09/example2.txt"
 
 print(part1(parse(data)))
 print(part2(parse(data)))
